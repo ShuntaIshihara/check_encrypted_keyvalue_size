3b51b52183829155a6adf666f447e7457c91856f
ShuntaIshihara <ishihara@kde.cs.tsukuba.ac.jp>
2021-06-14 00:56:06 +0000

    change buffer size

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <sgx_tcrypto.h>
 
-void *priv_key = malloc(256);
-void *pub_key = malloc(256);
+void *priv_key = NULL;
+void *pub_key = NULL;
 
 int ecall_generate_keys(const unsigned char *data)
 {
@@ -34,21 +34,22 @@ int ecall_generate_keys(const unsigned char *data)
     p_n, p_e, &pub_key);
 
     //暗号化
-    unsigned char *penc_data = (unsigned char *)malloc(256);
+    unsigned char *penc_data = (unsigned char *)malloc(1000);
     size_t *penc_len = (size_t *)malloc(sizeof(size_t));
     sgx_rsa_pub_encrypt_sha256(pub_key, penc_data, penc_len, (const unsigned char *)data, sizeof(data));
 
     ocall_enc_data(penc_data, penc_len);
 
     //複合化
-    unsigned char *pdec_data = (unsigned char *)malloc(256);
+    unsigned char *pdec_data = (unsigned char *)malloc(1000);
     size_t *pdec_len = (size_t *)malloc(sizeof(size_t));
     sgx_rsa_priv_decrypt_sha256(priv_key, pdec_data, pdec_len, penc_data, *penc_len);
 
     ocall_dec_data(pdec_data, pdec_len);
 
     free(priv_key); free(pub_key);
-    free(p_n); free(p_e), free(p_d), free(p_p); free(p_q); free(p_dmp1); free(p_dmq1); free(p_iqmp);
+    free(p_n); free(p_e); free(p_d); free(p_p); free(p_q); free(p_dmp1); free(p_dmq1); free(p_iqmp);
+    free(penc_data); free(pdec_data);
     //公開鍵だけ返す
 	return 1;
 }
